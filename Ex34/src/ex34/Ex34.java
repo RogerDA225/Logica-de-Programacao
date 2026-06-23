04f5b3f1338a58c615fd1c8db60e914585b3604c
RogerDA225 <roger.albineli@estudantes.ifc.edu.br>
2026-06-23 09:58:59 -0300

    Add files via upload

---
+++ Ex34/src/ex34/Ex34.java
@@ -0,0 +1,14 @@
+package ex34;
+
+public class Ex34 {
+
+    public static void main(String[] args) {
+        int a;
+        a = 0;
+        while (a <= 1000) {
+            System.out.println(a);
+            a = a + 1;
+        }
+    }
+
+}
---
+++ Ex35/src/ex35/Ex35.java
@@ -0,0 +1,18 @@
+
+package ex35;
+
+public class Ex35 {
+
+    public static void main(String[] args) {
+        int a;
+        a = 1000;
+        while (a <= 10000) {
+            if (a % 2 == 1) {
+                System.out.println(a);
+            }
+            a = a + 1;
+        }   
+        
+    }
+    
+}
